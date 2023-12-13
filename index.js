fd2037064add98c2709aa90bc78ab48a1c0d99be
Rohit <32985298+rohit-dalal@users.noreply.github.com>
2023-12-13 14:36:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let btn = document.getElementById('btn-search');
 
 
 function url_validate(url) {
-  var urlRegex =/(https?:\/\/(?:www\. |(?!www))[a-zA-Z0-9][a-zA-Z0-9- 5 ]+[a-zA-Z0-9]\;
+  var urlRegex =/^ ( (https?|ftp|smtp|http):\\/\\/)? (www.)? [a-z0-9]+ (\\. [a-z] {2,}) {1,3} (#?\\/? [a-zA-Z0-9#]+)*\\/? (\\? [a-zA-Z0-9-_]+= [a-zA-Z0-9-%]+&?)?$/;;
   if (url.match(urlRegex)){
     if (url.startsWith('https')){
       return url.split('://')
